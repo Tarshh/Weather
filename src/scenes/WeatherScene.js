845c5fe91a520a2f69767a27db70a9767cc6a09f
Tars Hoeykens <tarshoeykens@gmail.com>
2019-05-06 00:19:05 +0200

    Andere layout

--- src/scenes/WeatherScene.js
+++ src/scenes/WeatherScene.js
@@ -1,5 +1,7 @@
 import React from "react";
 import WeatherBlock from "../components/WeatherBlock";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 import "./weatherScene.css";
 
 export default class WeatherScene extends React.Component {
@@ -7,98 +9,75 @@ export default class WeatherScene extends React.Component {
     super(props);
     this.state = {
       loading: false,
-      items: []
+      items: [],
+      city: "Antwerp"
     };
+
+    this._onSelect = this._onSelect.bind(this);
   }
 
   fetchData = async () => {
-    const apiKey = "1c28d24177e115bd0233c20f72193c2f";
-    const city = "Antwerp";
+    // const apiKey = "1c28d24177e115bd0233c20f72193c2f";
+    const apiKey = "c55a767d38684c4fb37145650190305";
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&APPID=${apiKey}`
+      // `https://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&APPID=${apiKey}&units=metric`
+      `https://api.apixu.com/v1/current.json?key=${apiKey}&q=${this.state.city}`
     );
     const data = await response.json();
 
+    // const slicedItems = data.list.slice(0, 5);
     await this.setState({
       items: data
     });
   };
 
-  sliceData = () => {
-    const items = this.state.items;
-    const weatherItems = items.list;
-    const slicedItems = weatherItems.slice(0, 5);
-    this.setState({
-      items: slicedItems
-    });
-  };
-
-  async componentDidMount() {
-    this.setState({
+  async componentWillMount() {
+    await this.fetchData();
+    await this.setState({
       isLoaded: true
     });
 
-    await this.fetchData();
-    this.sliceData();
+    console.log(this.state.items);
   }
 
+  async _onSelect() {
+    console.log(this.state.city);
+  }
   render() {
-    const { items } = this.state;
-    console.log(items);
+    // const { items } = this.state;
+    const { city } = this.state;
+    const options = ["Antwerp", "Brussels", "Bruges"];
+
     return (
       <div>
-        <h1>Weather forecast Antwerp</h1>
+        <Dropdown
+          className="Dropdown"
+          options={options}
+          onChange={event => this._onSelect()}
+          value={city}
+          placeholder="Select an option"
+        />
         <div className="weatherContainer">
           <WeatherBlock
-            day="Monday"
+            day="Today"
             weatherIcon="cloud-lightning"
             alt="Cloud-lightning"
             highTemp="83"
             lowTemp="66°"
           />
-          <WeatherBlock
-            day="Tuesday"
-            weatherIcon="partly-cloudy-day"
-            alt="Partly-Cloudy"
-            highTemp="78"
-            lowTemp="67°"
-          />
-          <WeatherBlock
-            day="Wednesday"
-            weatherIcon="rainy-weather"
-            alt="Rainy-Weather"
-            highTemp="77"
-            lowTemp="65°"
-          />
-          <WeatherBlock
-            day="Thursday"
-            weatherIcon="summer"
-            alt="Summer"
-            highTemp="71"
-            lowTemp="61"
-          />
-          <WeatherBlock
-            day="Friday"
-            weatherIcon="sun"
-            alt="Sun"
-            highTemp="70"
-            lowTemp="60°"
-          />
-          <WeatherBlock
-            day="Saturday"
-            weatherIcon="windy-weather"
-            alt="Windy-Weather"
-            highTemp="73"
-            lowTemp="60°"
-          />
-          <WeatherBlock
-            day="Sunday"
-            weatherIcon="rain"
-            alt="Rain"
-            highTemp="69"
-            lowTemp="61°"
-          />
+          <div className="weatherInfoContainer">
+            <p className="WeatherInfoItem">
+              Local Time:<span> 21:35 </span>
+            </p>
+            <p className="WeatherInfoItem">
+              Humidity: <span>66%</span>
+            </p>
+            <p className="WeatherInfoItem">
+              Wind Speed: <span>25 kph</span>
+            </p>
+          </div>
         </div>
+        <p>Last updated on: 20:41 </p>
       </div>
     );
   }
