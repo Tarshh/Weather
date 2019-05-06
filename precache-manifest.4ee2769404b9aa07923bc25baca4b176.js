7bf747d6cd5484dec4e20a63e04b087424ffc5e2
Tars H <tars.hoeykens@crossworx.be>
2019-05-06 13:45:00 +0200

    Updates

---
+++ precache-manifest.4ee2769404b9aa07923bc25baca4b176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ff3c730dd846d0c1b1c8d327c409b5",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "5821f0778232694797f7",
+    "url": "/Weather/static/css/2.d58fcd6a.chunk.css"
+  },
+  {
+    "revision": "716960594c503c11a546",
+    "url": "/Weather/static/css/main.b4b91f45.chunk.css"
+  },
+  {
+    "revision": "5821f0778232694797f7",
+    "url": "/Weather/static/js/2.37c97077.chunk.js"
+  },
+  {
+    "revision": "716960594c503c11a546",
+    "url": "/Weather/static/js/main.39f8b6f2.chunk.js"
+  },
+  {
+    "revision": "e6172640d3267d8b322d",
+    "url": "/Weather/static/js/runtime~main.72be58e2.js"
+  }
+]);
